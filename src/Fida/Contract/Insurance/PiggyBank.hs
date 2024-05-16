57d5dc2861985688a2ffb816c66928bf4722942b
jankun4 <81573167+jankun4@users.noreply.github.com>
2024-05-16 22:27:46 +0200

    PiggyBank FidaCard-Sell and FidaCard-Buy

--- src/Fida/Contract/Insurance/PiggyBank.hs
+++ src/Fida/Contract/Insurance/PiggyBank.hs
@@ -1,19 +1,46 @@
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE NamedFieldPuns #-}
 
 module Fida.Contract.Insurance.PiggyBank (
     serialisablePiggyBankValidator,
 ) where
 
-import Fida.Contract.Insurance.Datum (PiggyBankDatum)
-import Fida.Contract.Insurance.Identifier (InsuranceId)
-import Fida.Contract.Insurance.Redeemer (PiggyBankRedeemer)
+import Fida.Contract.Insurance.Datum (PiggyBankDatum(..))
+import Fida.Contract.Insurance.Identifier (InsuranceId(..))
+import Fida.Contract.Insurance.Redeemer (PiggyBankRedeemer(..))
+import Fida.Contract.Insurance.Tokens (fidaCardTokenName, fidaCardStatusTokenName)
+import Fida.Contract.Utils (outputDatum, fromSingleton, lovelaceValueOf, output)
 import Plutus.V2.Ledger.Api
 import qualified PlutusTx
 import PlutusTx.Prelude
+import Plutus.V1.Ledger.Value
+import Plutus.V2.Ledger.Contexts (getContinuingOutputs, findOwnInput)
 
 newtype FidaCardId = FidaCardId Integer
     deriving newtype (ToData, FromData, UnsafeFromData)
 
+{- |
+
+ On chain errors:
+
+  ERROR-PIGGY-BANK-VALIDATOR-0: output datum doesn't say that the card is sold
+
+  ERROR-PIGGY-BANK-VALIDATOR-1: output datum doesn't match input datum
+
+  ERROR-PIGGY-BANK-VALIDATOR-2: own input not found
+
+  ERROR-PIGGY-BANK-VALIDATOR-3: correct output datum not found
+
+  ERROR-PIGGY-BANK-VALIDATOR-4: fida card not paid for
+
+  ERROR-PIGGY-BANK-VALIDATOR-5: output datum not found
+
+  ERROR-PIGGY-BANK-VALIDATOR-6: output datum says that the card is sold
+
+  ERROR-PIGGY-BANK-VALIDATOR-7: output datum doesn't match input datum
+
+  ERROR-PIGGY-BANK-VALIDATOR-8: datum not found
+-}
 {-# INLINEABLE mkPiggyBankValidator #-}
 mkPiggyBankValidator ::
     InsuranceId ->
@@ -22,7 +49,37 @@ mkPiggyBankValidator ::
     PiggyBankRedeemer ->
     ScriptContext ->
     Bool
-mkPiggyBankValidator _ _ _ _ _ = True
+mkPiggyBankValidator (InsuranceId cs) (FidaCardId n) (PBankFidaCard {pbfcIsSold=False, pbfcFidaCardValue}) BuyFidaCard scriptContext =
+        traceIfFalse "ERROR-PIGGY-BANK-VALIDATOR-0" pbfcIsSold
+        && traceIfFalse "ERROR-PIGGY-BANK-VALIDATOR-1" (pbfcFidaCardValue == pbfcFidaCardValue')
+    where
+        inputLovelace = case findOwnInput scriptContext of
+            Just (TxInInfo _ (TxOut _ value _ _)) -> lovelaceValueOf value
+            Nothing -> traceError "ERROR-PIGGY-BANK-VALIDATOR-2"
+
+        (PBankFidaCard {pbfcIsSold, pbfcFidaCardValue=pbfcFidaCardValue'}) =
+                case output cs scriptContext (fidaCardStatusTokenName) of
+                    Nothing -> traceError "ERROR-PIGGY-BANK-VALIDATOR-3"
+                    Just (TxOut _ v _ _, d) | lovelaceValueOf v < pbfcFidaCardValue + inputLovelace ->
+                        traceError "ERROR-PIGGY-BANK-VALIDATOR-4"
+                                            | otherwise -> d
+                    _ -> traceError "ERROR-PIGGY-BANK-VALIDATOR-5"
+mkPiggyBankValidator (InsuranceId cs) (FidaCardId n) (PBankFidaCard {pbfcIsSold=True, pbfcFidaCardValue}) SellFidaCard scriptContext =
+        traceIfFalse "ERROR-PIGGY-BANK-VALIDATOR-6" (not pbfcIsSold)
+        && traceIfFalse "ERROR-PIGGY-BANK-VALIDATOR-7" (pbfcFidaCardValue == pbfcFidaCardValue')
+    where datum = fromSingleton
+                    [ datum
+                    | TxOut _ value (OutputDatum (Datum d)) _ <- getContinuingOutputs scriptContext
+                    , valueOf value cs (fidaCardTokenName n) == 1
+                    , valueOf value cs fidaCardStatusTokenName == 1
+                    , Just datum <- [PlutusTx.fromBuiltinData d]
+                    ]
+          (PBankFidaCard {pbfcIsSold, pbfcFidaCardValue=pbfcFidaCardValue'}) =
+                case datum of
+                    Just x -> x
+                    Nothing -> traceError "ERROR-PIGGY-BANK-VALIDATOR-8"
+mkPiggyBankValidator _ _ _ _ _ = False
+
 
 {-# INLINEABLE mkPiggyBankValidatorUntyped #-}
 mkPiggyBankValidatorUntyped ::
