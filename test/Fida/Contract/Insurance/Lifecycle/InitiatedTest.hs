2849f36fcec682dd0ea1c94e4b0943ce4b25f21d
Sławomir Śledź <slawomir.sledz@gmail.com>
2024-06-03 23:37:11 +0200

    Reexport all submodules through TestToolbox module

--- test/Fida/Contract/Insurance/Lifecycle/InitiatedTest.hs
+++ test/Fida/Contract/Insurance/Lifecycle/InitiatedTest.hs
@@ -2,15 +2,13 @@
 
 module Fida.Contract.Insurance.Lifecycle.InitiatedTest (tests) where
 
-import Fida.Contract.TestToolbox.Users (Users(..), setupUsers)
-import Fida.Contract.TestToolbox.TypedValidators
-import Fida.Contract.TestToolbox.Action.MakeInsurancePolicy (newSamplePolicy)
+import Fida.Contract.TestToolbox (Users(..), setupUsers,
+                                  newSamplePolicy, runUpdatePolicyState, bad, good, Run)
 import Control.Monad (void)
-import Fida.Contract.Insurance.Datum
-import Fida.Contract.Insurance.InsuranceId
-import Fida.Contract.Insurance.Redeemer
-import Plutus.Model hiding (days)
-import Plutus.V2.Ledger.Api
+import Fida.Contract.Insurance.Datum (InsurancePolicyState (..))
+import Fida.Contract.Insurance.InsuranceId (InsuranceId)
+import Fida.Contract.Insurance.Redeemer (InsurancePolicyRedeemer (..), PolicyInitiatedRedemeer (..))
+import Plutus.V2.Ledger.Api (PubKeyHash)
 import Test.Tasty (TestTree, testGroup)
 import Prelude
 
@@ -25,31 +23,9 @@ tests =
     , bad "Cancelling policy can't set state to OnRisk" $ testCancelPolicyIfIllegalState OnRisk
     , bad "Cancelling policy can't set state to Initiated" $ testCancelPolicyIfIllegalState Initiated
     ]
- where
-  bad msg = good msg . mustFail
-  good = testNoErrors (adaValue 1000_000_000_000) defaultBabbage
 
 testCreatePolicy :: Run ()
 testCreatePolicy = void $ setupUsers >>= newSamplePolicy
-
-runUpdatePolicyState ::
-  InsurancePolicyState ->
-  InsurancePolicyRedeemer ->
-  InsuranceId ->
-  PubKeyHash ->
-  Run ()
-runUpdatePolicyState state r iid pkh = do
-  let tv = insurancePolicy iid
-  withBox @ InsurancePolicy (iinfoBox iid) tv $
-    \box@(TxBox _ (TxOut _ value _ _) iinfo) -> do
-      let maybeIinfo = updatePolicyState iinfo state
-      withMay "Can't update policy state" (pure maybeIinfo) $ \iinfo' -> do
-        let tx =
-              mconcat
-                [ spendBox tv r box
-                , payToScript tv (InlineDatum iinfo') value
-                ]
-        submitTx pkh tx
 
 cancelPolicy :: InsuranceId -> PubKeyHash -> Run ()
 cancelPolicy = runUpdatePolicyState Cancelled (PolicyInitiated PolicyInitiatedCancel)
--- test/Fida/Contract/TestToolbox.hs
+++ test/Fida/Contract/TestToolbox.hs
@@ -1,1 +1,20 @@
-module Fida.Contract.TestToolbox where
+module Fida.Contract.TestToolbox
+  ( module X
+  , bad
+  , good
+  , Run
+  ) where
+
+import Fida.Contract.TestToolbox.Action as X
+import Fida.Contract.TestToolbox.Users as X
+import Fida.Contract.TestToolbox.TypedValidators as X
+import Fida.Contract.TestToolbox.Time as X
+import Plutus.Model (Run, mustFail, testNoErrors, adaValue, defaultBabbage)
+import Test.Tasty (TestTree)
+import Prelude
+
+bad :: String -> Run a -> TestTree
+bad msg = good msg . mustFail
+
+good :: String -> Run a -> TestTree
+good = testNoErrors (adaValue 1000_000_000_000) defaultBabbage
