ceaff841fd46a283946f16cede782248483e82ad
Sławomir Śledź <slawomir.sledz@gmail.com>
2024-06-04 00:00:36 +0200

    Add placeholders for a tests

--- test/Main.hs
+++ test/Main.hs
@@ -2,6 +2,9 @@ module Main (main) where
 
 import qualified Fida.Contract.FidaPolicyContractTest as FidaPolicyContractTest
 import qualified Fida.Contract.Insurance.Lifecycle.InitiatedTest as InitiatedTest
+import qualified Fida.Contract.Insurance.PiggyBankTest as PiggyBankTest
+import qualified Fida.Contract.Insurance.Lifecycle.FundingTest as FundingTest
+import qualified Fida.Contract.Insurance.Lifecycle.OnRiskTest as OnRiskTest
 import Test.Tasty (TestTree, defaultMain, testGroup)
 import Prelude
 
@@ -14,4 +17,7 @@ tests =
     "Fida Contract Tests"
     [ FidaPolicyContractTest.tests
     , InitiatedTest.tests
+    , PiggyBankTest.tests
+    , FundingTest.tests
+    , OnRiskTest.tests
     ]
