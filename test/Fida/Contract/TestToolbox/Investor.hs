19704100bf058f87ebda95f652ab36ddf985e3ce
Sławomir Śledź <slawomir.sledz@gmail.com>
2024-06-13 23:19:23 +0200

    Investor tests stuff

--- test/Fida/Contract/TestToolbox/Investor.hs
+++ test/Fida/Contract/TestToolbox/Investor.hs
@@ -2,12 +2,16 @@
 
 module Fida.Contract.TestToolbox.Investor where
 
+import Control.Monad (join, filterM)
+import Data.Maybe (maybeToList)
 import Fida.Contract.Insurance.Datum
-import Fida.Contract.Insurance.InsuranceId (InsuranceId)
+import Fida.Contract.Insurance.InsuranceId (InsuranceId (..))
 import Fida.Contract.TestToolbox.TypedValidators
 import Plutus.Model
 import Plutus.V2.Ledger.Api
+import Plutus.V1.Ledger.Value (flattenValue)
 import Control.Monad (sequence)
+import qualified Data.ByteString as BS
 import Prelude
 
 data FidaCard = FidaCard InsuranceId FidaCardId deriving (Show, Eq)
@@ -52,6 +56,31 @@ withPiggyBankInfo card = withMay err (findPiggyBankInfo card)
     err = "Can't find piggy bank identified by " <> show card
 
 getInvestorPortfolio :: PubKeyHash -> Run InvestorPortfolio
-getInvestorPortfolio = do
+getInvestorPortfolio pkh = do
+  utxos <- utxoAt pkh
+  let fidaCardCandidates = getFidaCards utxos
+  investor'fidaCards <- filterM isFidaCard fidaCardCandidates
+  investor'contribution <- join <$> traverse getPiggyBankInfo investor'fidaCards
+  return InvestorPortfolio {..}
+  where
+    getPiggyBankInfo = fmap maybeToList . findPiggyBankInfo
 
-  undefined
+    getFidaCards utxos =
+      foldl (\acc (_, txOut) -> fidaCardsFromTxOut txOut <> acc) [] utxos
+
+    isFidaCard :: FidaCard -> Run Bool
+    isFidaCard (FidaCard iid fcid) = do
+      boxes <- boxAt $ piggyBank iid fcid
+      return $ not $
+        null [ ()
+             | box <- boxes
+             , piggyBankInfoBox iid fcid box
+             ]
+
+fidaCardsFromTxOut :: TxOut -> [FidaCard]
+fidaCardsFromTxOut (TxOut _ value _ _) =
+  [ FidaCard (InsuranceId cs) (FidaCardId tn)
+  | (cs, TokenName tn, n) <- flattenValue value
+  , n == 1
+  , BS.take 5 (fromBuiltin tn) == "CARD-"
+  ]
